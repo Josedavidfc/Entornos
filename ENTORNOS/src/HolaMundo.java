b3e3e5d9d612c5d22904dc16b69f9872906ebb3a
josedavidfernandezcomino <josedavidfernandezcomino@localhost>
2020-11-11 19:24:48 +0100

    Primer proyecto.

---
+++ ENTORNOS/src/HolaMundo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author josedavidfernandezcomino
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
